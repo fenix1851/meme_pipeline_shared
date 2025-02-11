957b04d02b723d58e7e52c36d9ef4aa4c2fd6d88
Your Name <you@example.com>
2025-02-11 15:47:43 +0100

    fix: update foreign key reference in RedditPost model to SubRedditID

--- models/reddit_posts.go
+++ models/reddit_posts.go
@@ -8,13 +8,12 @@ type RedditPost struct {
 	PostTitle string `gorm:"column:post_title;type:text;not null"`
 	PostText  string `gorm:"column:post_text;type:text"`
 	Upvotes   int    `gorm:"column:upvotes"`
-	// Связь с reddit_treads
-	SubRedditId uint       `gorm:"column:post_subreddit"`
-	SubReddits  SubReddits `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	Comments    int        `gorm:"column:comments"`
-
-	CreatedAt time.Time `gorm:"column:created_at"`
-	UpdatedAt time.Time `gorm:"column:updated_at"`
+	// Внешний ключ для связи с сабреддитом
+	SubRedditID uint        `gorm:"column:post_subreddit"`
+	SubReddit   *SubReddits `gorm:"foreignKey:SubRedditID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	Comments    int         `gorm:"column:comments"`
+	CreatedAt   time.Time   `gorm:"column:created_at"`
+	UpdatedAt   time.Time   `gorm:"column:updated_at"`
 	// Один Reddit-пост может быть привязан к нескольким темам мема
 	MemeTopics []MemeTopic `gorm:"foreignKey:RedditPostID"`
 }
--- models/reddit_tread.go
+++ models/reddit_tread.go
@@ -8,6 +8,6 @@ type SubReddits struct {
 	TreadLink string    `gorm:"column:tread_link;type:text;not null"`
 	CreatedAt time.Time `gorm:"column:created_at"`
 	UpdatedAt time.Time `gorm:"column:updated_at"`
-	// Один тред может быть привязан к нескольким темам мема
-	RedditPosts []RedditPost `gorm:"foreignKey:SubRedditId"` // updated foreign key reference
+	// Один тред может быть привязан к нескольким постам
+	RedditPosts []RedditPost `gorm:"foreignKey:SubRedditID"`
 }
