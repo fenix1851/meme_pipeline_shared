5c6fe101cea1c820a7989632b73eaa8a32655a84
Your Name <you@example.com>
2025-02-12 08:06:51 +0100

    feat: add AdditionalInfo field to MemeTemplate model for enhanced data storage

--- models/meme_template.go
+++ models/meme_template.go
@@ -5,10 +5,11 @@ import (
 )
 
 type MemeTemplate struct {
-	ID            uint           `gorm:"primaryKey"`
-	PicPath       string         `gorm:"column:pic_path;type:text;not null"`
-	TextContent   datatypes.JSON `gorm:"column:text_content;type:jsonb"`
-	IsProccessing bool           `gorm:"column:is_proccessing;default:false"`
+	ID             uint           `gorm:"primaryKey"`
+	PicPath        string         `gorm:"column:pic_path;type:text;not null"`
+	AdditionalInfo datatypes.JSON `gorm:"column:additional_info;type:jsonb"`
+	TextContent    datatypes.JSON `gorm:"column:text_content;type:jsonb"`
+	IsProccessing  bool           `gorm:"column:is_proccessing;default:false"`
 	// Один шаблон может использоваться в нескольких сгенерированных мемах
 	GeneratedMemes []GeneratedMeme `gorm:"foreignKey:MemeTemplateID"`
 }
