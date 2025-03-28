bd54cc5b742d24a5bb5ff5e5e52c81f49c50baf9
Tolik <laktolik9o@gmail.com>
2025-03-28 19:38:33 +0100

    feat: refactor YouTube message structures and add RabbitMQ message types

--- messages/youtubeMessages.go
+++ messages/youtubeMessages.go
@@ -1,24 +1,15 @@
 package messages
 
-import "time"
-
-type YoutubeVideo struct {
-	ID            int            `gorm:"column:id;primaryKey;autoIncrement"`
-	ChannelID     int            `gorm:"column:channel_id;not null"`
-	Channel       YoutubeChannel `gorm:"foreignKey:ChannelID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	MinioID       string         `gorm:"column:minio_id"`
-	URL           string         `gorm:"column:url"`
-	VideoSections []string       `gorm:"column:video_sections;type:text[]"`
-	VideoLength   string         `gorm:"column:video_length"` // Representing SQL INTERVAL as a string
-	CreatedAt     time.Time      `gorm:"column:created_at"`
-	UpdatedAt     time.Time      `gorm:"column:updated_at"`
+type RabitUrlMessage struct {
+	Type string `json:"type"`
+	Url  string `json:"url"`
 }
 
-type YoutubeChannel struct {
-	ID           int       `gorm:"column:id;primaryKey;autoIncrement"`
-	ChannelUrl   string    `gorm:"column:link"`
-	Subscribers  int       `gorm:"column:subscribers"`
-	OriginalData string    `gorm:"column:original_data"`
-	CreatedAt    time.Time `gorm:"column:created_at"`
-	UpdatedAt    time.Time `gorm:"column:updated_at"`
+const YoutubeUrlType = "youtube_url"
+
+type RabbitChannelMessage struct {
+	Type string `json:"type"`
+	Url  string `json:"url"`
 }
+
+const YoutubeChannelType = "youtube_channel"
